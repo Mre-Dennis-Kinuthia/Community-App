8d649603587fe14faa74c42e346573b2f6e62f4e
mre-dennis-kinuthia <denniskinuthia1105@gmail.com>
2026-01-18 22:04:51 +0300

    deployment issu fix

--- frontend/app/news/[id]/page.tsx
+++ frontend/app/news/[id]/page.tsx
@@ -18,7 +18,6 @@ import {
 import { format } from "date-fns"
 import Link from "next/link"
 import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 
